96b7deeb879679f94bf5e236333a0b9bf15999b5
dshvedchenko <dshvedchenko@gmail.com>
2016-08-31 13:36:32 +0300

    add ex 1

---
+++ tomcat/recipes/default.rb
@@ -0,0 +1,46 @@
+
+package 'java-1.7.0-openjdk-devel'
+
+group 'tomcat'
+
+user 'tomcat' do
+  shell '/bin/nologin'
+  home '/opt/tomcat'
+  manage_home false
+  group 'tomcat'
+end
+
+remote_file '/opt/apache-tomcat-8.0.36.tar.gz' do
+  source 'http://apache.volia.net/tomcat/tomcat-8/v8.0.36/bin/apache-tomcat-8.0.36.tar.gz'
+  mode '0755'
+  action :create
+end
+
+directory '/opt/tomcat' do
+  action :create
+end
+
+execute 'tar xvf /opt/apache-tomcat-8.0.36.tar.gz -C /opt/tomcat --strip-components=1'
+
+execute 'chgrp -R tomcat /opt/tomcat/conf'
+
+directory '/opt/tomcat/conf' do
+  mode '0070'
+end
+
+execute 'chmod g+r /opt/tomcat/conf/*'
+
+%w[ webapps work temp logs ].each do |path|
+  execute "chown -R tomcat /opt/tomcat/#{path}"
+end
+
+template '/etc/systemd/system/tomcat.service' do
+  source 'tomcat.service.erb'
+end
+
+#TODO: NOT DESIRED
+execute 'systemctl daemon-reload'
+
+service 'tomcat' do
+  action [:start, :enable]
+end
---
+++ tomcat/test/recipes/default_test.rb
@@ -0,0 +1,45 @@
+# # encoding: utf-8
+
+# Inspec test for recipe tomcat::default
+
+# The Inspec reference, with examples and extensive documentation, can be
+# found at https://docs.chef.io/inspec_reference.html
+
+describe bash('curl http://localhost:8080') do
+  its('stdout') { should match /Tomcat/ }
+end
+
+describe package('java-1.7.0-openjdk-devel') do
+  it { should be_installed }
+end
+
+describe group('tomcat') do
+  it { should exist }
+end
+
+describe user('tomcat') do
+  its('group') { should eq 'tomcat' }
+  its('home') { should eq '/opt/tomcat' }
+  its('shell') { should eq '/bin/nologin' }
+end
+
+describe file ('/opt/tomcat') do
+  it { should exist }
+  it { should be_directory }
+end
+
+describe file('/opt/tomcat/conf') do
+  it { should exist }
+  its('mode') { should eq 0070 }
+end
+
+%w[ webapps work temp logs].each do |path|
+  describe file("/opt/tomcat/#{path}") do
+    it { should exist }
+    it { should be_owned_by 'tomcat'}
+  end
+end
+
+describe file('/etc/systemd/system/tomcat.service') do
+  it { should exist }
+end
