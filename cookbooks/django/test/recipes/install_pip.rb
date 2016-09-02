3f9a5ca6f7d845156e4d815c9ace55d244f25de2
dshvedchenko <dshvedchenko@gmail.com>
2016-09-02 09:29:25 +0300

    add suite install-pip-

--- cookbooks/django/test/recipes/install_pip.rb
+++ cookbooks/django/test/recipes/install_pip.rb
@@ -1,10 +1,6 @@
 # # encoding: utf-8
 
-# Inspec test for recipe django::default
+# Inspec test for recipe django::install_pip
 
 # The Inspec reference, with examples and extensive documentation, can be
 # found at https://docs.chef.io/inspec_reference.html
-
-describe command('django-admin --version') do
-  its(:stdout) { should match /1.10.1/ }
-end
