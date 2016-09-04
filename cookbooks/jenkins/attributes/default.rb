25f5447721644ad22816173608e8ea01a0a2c264
dshvedchenko <dshvedchenko@gmail.com>
2016-09-04 07:03:18 +0300

    work with comminuty cookbook, to get some parts and way of implemetation

---
+++ cookbooks/jenkins/attributes/default.rb
@@ -0,0 +1,3 @@
+default['jenkins']['master']['repository'] = 'http://pkg.jenkins-ci.org/debian'
+default['jenkins']['master']['repository_key'] = 'http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key'
+default['jenkins']['master']['repository_keyserver'] = nil
--- cookbooks/jenkins/recipes/default.rb
+++ cookbooks/jenkins/recipes/default.rb
@@ -4,25 +4,16 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
-remote_file '/tmp/jenkins-ci.org.key' do
-  source 'https://pkg.jenkins.io/debian/jenkins-ci.org.key'
-  notifies :run, 'execute[add_jenkins_key]', :immediately
-end
+#include_recipe 'apt::default' -- already in chef
 
-execute 'add_jenkins_key' do
-  action :nothing
-  command 'apt-key add /tmp/jenkins-ci.org.key'
-end
-
-file '/etc/apt/sources.list.d/jenkins.list' do
-  action :nothing
-  content 'deb http://pkg.jenkins.io/debian-stable binary/'
-  notifies :run, 'execute[apt-get update]', :immediately
-end
-
-execute 'apt-get update' do
-  action :nothing
-end
+apt_repository 'jenkins' do
+    uri          node['jenkins']['master']['repository']
+    distribution 'binary/'
+    key          node['jenkins']['master']['repository_key']
+    unless node['jenkins']['master']['repository_keyserver'].nil?
+      keyserver    node['jenkins']['master']['repository_keyserver']
+    end
+  end
 
 package 'openjdk-7-jre-headless'
 package 'jenkins'
